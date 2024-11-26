e59934f246f64da683c66c9192a433de2496b48d
Guillermo Tabligan <guillermotabligan@gmail.com>
2024-11-26 14:51:01 +0000

    fix: base URL

--- frontend/src/api/productApi.ts
+++ frontend/src/api/productApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Product } from '../types/Product';
 
-const API_BASE_URL = 'http://backend.sourcefit.local/api';
+const API_BASE_URL = 'http://178.128.101.196:8080/api';
 
 export const fetchProducts = async (): Promise<Product[]> => {
     const response = await axios.get(`${API_BASE_URL}/products`);
